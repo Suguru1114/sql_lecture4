9cd1627d1d20fe8303e1820523a70458d8130990
Suguru Miyata <suguru1114@gmail.com>
2023-08-02 21:49:26 +1000

    add new class airport

--- flights/models.py
+++ flights/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.id}:{self.origin} to {self.destination}"
+
 class Flight(models.Model):
     origin = models.CharField(max_length=64)
     destination = models.CharField(max_length=100, default='Default Destination')
